e91d059c1a566f020bf7de79ec2617e1f669261c
Odyseja1602 <odyseja16@gmail.com>
2016-04-27 21:53:58 +0200

    final fixes

--- copernicus_client.py
+++ copernicus_client.py
@@ -7,6 +7,8 @@ import mosquitto
 import serial
 from threading import Thread
 
+current_position = 0
+
 
 def on_connect(mqttc, obj, rc):
     print("rc: "+str(rc))
@@ -17,6 +19,8 @@ def on_message(mqttc, obj, msg):
     ser = serial.Serial('/dev/ttyS0', 38400, timeout=1)
     if position:
         ser.write(chr(position))
+    global curr
+    curr=developers[str(msg.payload)]
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
 
 
@@ -30,8 +34,6 @@ def on_subscribe(mqttc, obj, mid, granted_qos):
 
 def on_log(mqttc, obj, level, string):
     print(string)
-
-developers = {"Maciek":6, "Katarzyna":22, "success":0}
 
 
 def turn_green_light():
@@ -49,6 +51,7 @@ def turn_blue_light():
     ser.write(chr(64+2+1))
 
 
+
 def find_position(name):
     if name == "success":
         turn_green_light()
@@ -58,29 +61,45 @@ def find_position(name):
         return None
     else:
         turn_blue_light()
-    return developers[name]
+    return count_num(name)
+
+def count_num(name):
+    print name
+    num = developers[name]
+    if (developers[my_name]+1)%3 == num: # left
+        res = 25
+    else:
+        res = 5
+    return res
+
 
 # If you want to use a specific client id, use
 # mqttc = mqtt.Client("client-id")
 # but note that the client id must be unique on the broker. Leaving the client
 # id parameter empty will generate a random id for you.
-mqttc = mosquitto.Mosquitto() 
+mqttc = mosquitto.Mosquitto()
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 
-if len(sys.argv) < 6:
-    print("Wrong amount of arguments: python "+sys.argv[0]+" address port")
+developers = {"Passarinho": 1, "Wegrzyns":2, "Odyseja":3}
+
+curr = 0
+curr_position = 15
+
+
+if len(sys.argv) < 5:
+    print("Wrong amount of arguments: python "+sys.argv[0]+" address port name")
     sys.exit(1)
 
 turn_green_light()
+ser = serial.Serial('/dev/ttyS0', 38400, timeout=1)
+ser.write(chr(15))
 
 broker_address = str(sys.argv[1])
 broker_port = int(sys.argv[2])
 my_name = str(sys.argv[3])
-my_position = int(sys.argv[4])
-hom_many_devs = int(sys.argv[5])
 
 mqttc.connect(broker_address, broker_port, 60)
 
@@ -88,4 +107,4 @@ mqttc.subscribe("temp/blamer", 0)
 
 mqttc.loop_forever()
 
- 
+
